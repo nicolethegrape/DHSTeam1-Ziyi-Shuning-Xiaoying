c859c02da477d74939a290ac9eff0bee3e4cca29
XiaoyingYu <xiy62@pitt.edu>
2017-03-30 10:54:33 -0400

    adjust 2nd

--- Graph/TFplot.R
+++ Graph/TFplot.R
@@ -4,9 +4,13 @@ generateTFPlot<-function(a,b,c,d,e,f){
   numService<-c(a,b,c,d,e)
   nameService<-c(rep("Total",1549),rep("Housing",1549),rep("Behavior",1549),rep("Nutrition",1549),rep("Mental",1549))
   placed<-rep(f,5)
-  datPlot<-data.frame(numService,nameService,placed)
-  p<-ggplot(datPlot, aes(as.factor(nameService), numService)) 
-  p+ geom_boxplot(varwidth = TRUE, outlier.colour = "grey",ylab="numServiceFamily")+facet_grid(. ~ placed)
+  datPlot<-data.frame(numService,services=as.factor(nameService),placed)
+  p<-ggplot(datPlot, aes(services, numService)) 
+  p+ geom_boxplot(varwidth = TRUE, outlier.colour = "grey")+
+  facet_grid(.~placed)+
+  ylab("numServiceFamily")+
+  ggtitle("Number of Services and Children Placement Comparison Graph")
+ 
 
 }
 
