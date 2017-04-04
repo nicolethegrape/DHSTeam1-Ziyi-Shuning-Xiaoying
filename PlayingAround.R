702e84408fb4405a01a10ff47a56b236bf3728ec
ShuningTong <sht72@pitt.edu>
2017-04-04 12:30:08 -0400

    modify PlayingAround

--- PlayingAround.R
+++ PlayingAround.R
@@ -1,3 +1,5 @@
+
+# example
 install.packages("reshape")
 library(reshape)
 library(ggplot2)
@@ -10,9 +12,7 @@ dfr <- data.frame(
   end.date    = c("2014-08-07 09:03:28.300", "2014-08-07 09:03:30.409")
 )
 
-
 mdfr <- melt(dfr, measure.vars = c("start.date", "end.date"))
-
 
 mdfr$time<-as.POSIXct(mdfr$value)
 
@@ -23,18 +23,19 @@ ggplot(mdfr, aes(time,name)) +
   scale_x_datetime(breaks=date_breaks("2 sec"),
                    limits = as.POSIXct(c('2014-08-07 09:03:24','2014-08-07 09:03:29')))
 
+# my own effort
 rm(list = ls())
-setwd("~/Desktop/Capstone/DHSTeam1-Ziyi-Shuning-Xiaoying") # change to where you put DHSTeam1 folder
 library(readxl)
-dat2 <- read_excel("Data/DHS_CrossSystem.xlsx",
-                   sheet = "SystemInvolvement_EC2016") # 8206 obs
+library(reshape)
 library(ggplot2)
 library(dplyr)
 library(scales)
+setwd("~/Desktop/Capstone/DHSTeam1-Ziyi-Shuning-Xiaoying") # change to where you put DHSTeam1 folder
+dat2 <- read_excel("Data/DHS_CrossSystem.xlsx",
+                   sheet = "SystemInvolvement_EC2016") # 8206 obs
 dat688929 <- filter(dat2, CLIENT_ID == 688929)
 dat688929clean <- dat688929[, colSums(is.na(dat688929)) < nrow(dat688929)]
 dat688929clean <- data.frame(dat688929clean)
-library(reshape)
 mdat688929clean <- melt(dat688929clean, measure.vars = names(dat688929clean)[-1])
 mdat688929clean$value <- paste0("01-", mdat688929clean$value)
 mdat688929clean$value <- as.Date(mdat688929clean$value, "%d-%B-%Y")
