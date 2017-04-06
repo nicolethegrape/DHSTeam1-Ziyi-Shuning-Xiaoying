b2244cad4f6c82ec38a50977ea6dba063df77983
nicolethegrape <ziz20@pitt.edu>
2017-04-06 10:37:30 -0400

    generate kernel density plot

--- Graph/KernelDensityPlot.R
+++ Graph/KernelDensityPlot.R
@@ -1,4 +1,7 @@
-generateKernelDensity <- function(finalData, xVarName){
-  ggplot(data=finalData, aes_string(x = xVarName,fill=placement)) + 
-    geom_density(kernel = "gaussian",alpha=0.3) 
+#generate kernel density graph 
+generateKernelDensity <- function(finalData, xVarName,filter){
+  p<-ggplot(data=finalData, aes_string(x = xVarName,fill=filter)) 
+  p+geom_density(alpha=0.3)+
+    ggtitle("Concurreny Serives and Children Placement Comparison Density Graph")+
+    theme_bw()
 }
