336fc294ef8a37f57d242ba42e8a0b1b93c55117
XiaoyingYu <xiy62@pitt.edu>
2017-04-05 21:06:22 -0400

    2nd update

--- Graph/KernelDensityPlot.R
+++ Graph/KernelDensityPlot.R
@@ -1,5 +1,4 @@
 generateKernelDensity <- function(finalData, xVarName){
-  ggplot(data=finalData, aes_string(x = xVarName)) + 
-    geom_density(kernel = "gaussian") + 
-    facet_grid(.~placement)
+  ggplot(data=finalData, aes_string(x = xVarName,fill=placement)) + 
+    geom_density(kernel = "gaussian",alpha=0.3) 
 }
