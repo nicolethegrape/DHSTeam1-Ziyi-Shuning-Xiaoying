4936d089acc18c3a601ab64f3ee18eefa84d1458
nicolethegrape <ziz20@pitt.edu>
2017-04-08 12:56:42 -0400

    change columns with accept dates

--- Functions/KeepOnlyService.R
+++ Functions/KeepOnlyService.R
@@ -2,6 +2,6 @@
 
 library(dplyr)
 keepOnlyService <- function(mergedData){
-  mergedData <- mergedData%>% 
-    select(c(CLIENT_ID:CASE_ID, CL_INLV_START, LAST_CLOSE_DT:ID_MAX_ACTIVE))   
+  mergedData <- mergedData %>% 
+    select(c(CLIENT_ID:CASE_ID, SRVC_ACPT_DT_MY, ACHA_MIN_ACTIVE:ID_MAX_ACTIVE))   
 }
