a00bde68067039f30b147d921510ef796005fcd5
nicolethegrape <ziz20@pitt.edu>
2017-04-08 13:02:48 -0400

    change columns

--- Functions/ConvertDate.R
+++ Functions/ConvertDate.R
@@ -3,7 +3,7 @@
 convertDate<-function(mergedData){
   library(zoo)
   DateDat<-mergedData
-  for(i in c(3,5:46)){
+  for(i in c(3:45)){
     DateDat[,i]<-as.Date(as.yearmon(DateDat[,i],"%b-%Y"))
   }
   return(DateDat)
