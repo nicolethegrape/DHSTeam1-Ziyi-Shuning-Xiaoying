ee6c0b7a8184c41cb5e828050a74a2d7e648d865
ShuningTong <sht72@pitt.edu>
2017-04-10 12:46:17 -0400

    change Main3 because "concurrency" is no longer in use

--- Main3.R
+++ Main3.R
@@ -7,32 +7,32 @@ mergedData <- read.csv("Data/MergedData.csv")
 
 placeData <- read.csv("Data/Placement.csv")
 
-
 # source("Functions/KeepOnlyService.R")
 # cleanedData <- keepOnlyService(mergedData)
 # 
 # source("Functions/ConvertDate.R")
 # convertedData <- convertDate(cleanedData)
 # 
-# source("Functions/CalConcurrencyForClient.R")
-# concurrencyDataFrame <- calConcurrencyForClient(convertedData)
+# source("Functions/CalServiceBA.R")
+# serviceBAData <- calServiceBA(convertedData)
+
+
+### no longer in use ###
+
+# concurrencyDataFrame <- read.csv("Data/ConcurrencyDataFrame.csv")
 # 
-# write.csv(concurrencyDataFrame, "Data/ConcurrencyDataFrame.csv", row.names=FALSE)
+# 
+# source("Functions/CalConcurrencyRatio.R")
+# concurrencyRatio <- calConcurrencyRatio(concurrencyDataFrame)
+# 
+# source("Functions/MergePlaceAndService.R")
+# finalData <- mergePlaceAndService(placeData, concurrencyRatio)
+# 
+# 
+# library(ggplot2)
+# source("Graph/KernelDensityPlot.R")
+# generateKernelDensity(finalData, "ACHA","placement")
+# generateKernelDensity(finalData, "MH","placement")
+# generateKernelDensity(finalData, "DA","placement")
+# generateKernelDensity(finalData, "DPW_FS","placement")
 
-
-concurrencyDataFrame <- read.csv("Data/ConcurrencyDataFrame.csv")
-
-
-source("Functions/CalConcurrencyRatio.R")
-concurrencyRatio <- calConcurrencyRatio(concurrencyDataFrame)
-
-source("Functions/MergePlaceAndService.R")
-finalData <- mergePlaceAndService(placeData, concurrencyRatio)
-
-
-library(ggplot2)
-source("Graph/KernelDensityPlot.R")
-generateKernelDensity(finalData, "ACHA","placement")
-generateKernelDensity(finalData, "MH","placement")
-generateKernelDensity(finalData, "DA","placement")
-generateKernelDensity(finalData, "DPW_FS","placement")
