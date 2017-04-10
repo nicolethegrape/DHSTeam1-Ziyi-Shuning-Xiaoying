d2a592e967b4185ad03770c5740142ab16b1995e
ShuningTong <sht72@pitt.edu>
2017-04-10 17:04:01 -0400

    flip the colnames "Duration" "CloseTimes"

--- Main4.R
+++ Main4.R
@@ -1,6 +1,5 @@
 setwd("~/Desktop/Capstone/DHSTeam1-Ziyi-Shuning-Xiaoying") # change to where you put DHSTeam1 folder
 rm(list = ls())
-library(zoo)
 
 # mergedData <- read.csv("Data/MergedData.csv")
 # 
