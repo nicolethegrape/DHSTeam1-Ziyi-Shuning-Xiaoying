400be1c688161767a56638595dba19018be697f8
ShuningTong <sht72@pitt.edu>
2017-04-11 15:25:47 -0400

    add TypeCountsFinalData.csv

--- Main5_TypeCountsAsX.R
+++ Main5_TypeCountsAsX.R
@@ -2,4 +2,17 @@ setwd("~/Desktop/Capstone/DHSTeam1-Ziyi-Shuning-Xiaoying") # change to where you
 rm(list = ls())
 library(dplyr)
 
-familyPreCYFData <- read.csv("Data/Data/FamilyPreCYFData.csv")
+# familyPreCYFData <- read.csv("Data/Data/FamilyPreCYFData.csv")
+# 
+# source("Functions/CalTypeCounts.R")
+# typeCountsData <- calTypeCounts(familyPreCYFData)
+
+typeCountsData <- read.csv("Data/TypeCountsData.csv")
+
+familyPlacePostCYFData <- read.csv("Data/FamilyPlacePostCYFData.csv")
+durationAndCloseTimes <- read.csv("Data/DurationAndCloseTimes.csv")
+
+source("Functions/MergeXYVars.R")
+typeCountsFinalData <- mergeXYVars(typeCountsData, familyPlacePostCYFData, durationAndCloseTimes)
+
+# typeCountsFinalData <- read.csv("Data/TypeCountsFinalData.csv")
