93077619df95b86f44eaa3ea32cec8c39e70b067
ShuningTong <sht72@pitt.edu>
2017-04-11 17:25:59 -0400

    modify Main5

--- Main5_TypeCountsAsX.R
+++ Main5_TypeCountsAsX.R
@@ -16,3 +16,41 @@ source("Functions/MergeXYVars.R")
 typeCountsFinalData <- mergeXYVars(typeCountsData, familyPlacePostCYFData, durationAndCloseTimes)
 
 # typeCountsFinalData <- read.csv("Data/TypeCountsFinalData.csv")
+
+
+
+## Temporarily put graph codes here
+
+# bar plot about types of services and placement
+library(ggplot2)
+ggplot(typeCountsFinalData, aes(TypeCounts, fill = PlacementAsY, order = -as.numeric(PlacementAsY))) + 
+  geom_bar(stat = "bin", position = "fill", alpha=0.6) + 
+  xlab("Types of Services") +
+  ylab("Place / Not Place Percentages") +
+  ggtitle("Types of Services and Placement")
+
+
+# bar plot about types of services and close times
+# change CloseTimes to factor format
+typeCountsFinalData$CloseTimes <- as.factor(typeCountsFinalData$CloseTimes)
+
+ggplot(typeCountsFinalData, aes(TypeCounts, fill = CloseTimes, order = -as.numeric(CloseTimes))) + 
+  geom_bar(stat = "bin", position = "fill", alpha=0.6) + 
+  scale_fill_brewer(palette = "Blues") + 
+  xlab("Types of Services") +
+  ylab("CloseTimes") +
+  ggtitle("Types of Services and CloseTimes")
+
+# kernel density plot about types of services and duration
+# change TypeCounts to factor format
+typeCountsFinalData$TypeCounts <- as.factor(typeCountsFinalData$TypeCounts)
+
+ggplot(typeCountsFinalData, aes(TypeCounts, Duration)) + 
+  geom_boxplot()
+
+ggplot(typeCountsFinalData, aes(Duration, fill = TypeCounts)) + 
+  geom_density(alpha=0.3)+
+  scale_fill_brewer(palette = "Blues") + 
+  ggtitle("Types of Services and Duration")+
+  theme_bw() +
+  facet_wrap(~TypeCounts)
