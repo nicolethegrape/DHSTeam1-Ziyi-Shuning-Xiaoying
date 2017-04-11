e2cf3a68bb149fc1e30f34f1f7601526e3f8887a
ShuningTong <sht72@pitt.edu>
2017-04-11 15:10:51 -0400

    rename Main files

--- Main4_BinaryAsX.R
+++ Main4_BinaryAsX.R
@@ -1,6 +1,6 @@
 setwd("~/Desktop/Capstone/DHSTeam1-Ziyi-Shuning-Xiaoying") # change to where you put DHSTeam1 folder
 rm(list = ls())
-
+library(dplyr)
 # mergedData <- read.csv("Data/MergedData.csv")
 # 
 # source("Functions/KeepOnlyService.R")
@@ -18,10 +18,10 @@ serviceBAData <- read.csv("Data/ServiceBAData.csv")
 
 # source("Functions/CalFamilyPreCYF.R")
 # familyPreCYFData <- calFamilyPreCYF(serviceBAData)
+# write.csv(familyPreCYFData, "Data/FamilyPreCYFData.csv", row.names=FALSE)
 # 
 # source("Functions/AggrFamilyPreCYFCat.R")
 # familyPreCYFCatData <- aggrFamilyPreCYFCat(familyPreCYFData)
-# 
 # write.csv(familyPreCYFCatData, "Data/FamilyPreCYFCatData.csv", row.names=FALSE)
 
 familyPreCYFCatData <- read.csv("Data/FamilyPreCYFCatData.csv")
---
+++ Main5_TypeCountsAsX.R
@@ -0,0 +1,5 @@
+setwd("~/Desktop/Capstone/DHSTeam1-Ziyi-Shuning-Xiaoying") # change to where you put DHSTeam1 folder
+rm(list = ls())
+library(dplyr)
+
+familyPreCYFData <- read.csv("Data/Data/FamilyPreCYFData.csv")
